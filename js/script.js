41922119b86ef67c4bb763827aa697fdc1b932c4
Alice Frumin <alifrumin@yahoo.com>
2015-12-26 00:52:57 -0500

    done for the night

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   console.log("ready!");
   $(".feb").on("click", function() {
-    $(".feb").html('<div class="febshow"><h4>February</h4>Valentines Day with Monica, Ben and Joey. Homemade pizza and pasta</div>')
+    $(".feb").html('<div class="febshow"><h4>February</h4><p>Valentines Day with Monica, Ben and Joey. Homemade pizza and pasta</p></div>')
   })
   $(".mar").click(function() {
     $(".mar").html("");
