1681865fee0af1d1215ed267983021376eacdf95
Alice Frumin <alifrumin@yahoo.com>
2016-01-04 19:48:12 -0500

    for pages

--- js/script.js
+++ js/script.js
@@ -5,11 +5,10 @@ $(document).ready(function() {
     $(".feb").html('<div class="febshow"><h4>February</h4><p>Valentines Day with Monica, Ben and Joey. Homemade pizza and pasta</p></div>')
   })
   $(".mar").click(function() {
-    // $(".mar").html("");
-    $(".mar").toggle(".marshow");
-
+alert("March: Pancake Tuesday with Jophie")
   });
   $(".apr").click(function(){
     $(".apr").toggleClass("clicked")
+    $(".apr").removeClass("apr")
   })
 });
